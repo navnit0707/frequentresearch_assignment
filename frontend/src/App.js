352f5219154b6fd49bd719a846443eddd995c179
navnit0707 <knavnit81@gmail.com>
2023-06-08 16:57:29 +0530

    added basic structure and tailwind to style

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-red-500 text-white py-4 px-6 rounded-lg">
+        Hello Navnit !
+      </div>
+    </div>
+  );
+}
+
+export default App;
